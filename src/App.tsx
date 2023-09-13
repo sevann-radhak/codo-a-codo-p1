da977fa5192546bd2237a9167b8c7b81e7ad0c3b
Sevann Radhak <sevann.radhak@gmail.com>
2023-09-13 18:03:20 -0300

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Index from './components/Index';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import Contact from './components/Contact';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+            <li>
+              <Link to="/page3">Page 3</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index: React.FC = () => {
+    return (
+        <div>
+            <h1>Index Page</h1>
+            <p>This is the Index page content.</p>
+        </div>
+    );
+};
+
+export default Index;
