c12f010d0bde66d533d0741cdc29d43852dae9c3
Sevann Radhak <sevann.radhak@gmail.com>
2023-09-19 20:08:38 -0300

    changes

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            <h1>This is the Header!</h1>
+        </div>
+    );
+};
+
+export default Header;
